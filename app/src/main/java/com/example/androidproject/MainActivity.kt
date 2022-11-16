8a6b35fabde2cb05ce24c25619467a7714e33152
IgorBr777 <igorbraz14@gmail.com>
2022-11-16 22:16:58 +0300

    Lesson16 AndroidActivity, ActionBar, Menu

--- app/src/main/java/com/example/androidproject/MainActivity.kt
+++ app/src/main/java/com/example/androidproject/MainActivity.kt
@@ -1,15 +1,10 @@
 package com.example.androidproject
 
 import android.content.Intent
-import android.graphics.Color
-import android.graphics.drawable.ColorDrawable
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
 import android.widget.Button
 import android.widget.TextView
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.androidproject.MainActivity2.Companion.startMainActivity2
 
 
@@ -18,50 +13,29 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-startMainActivity2(this, getString(R.string.hello_world))
-                supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        startMainActivity2(this, getString(R.string.hello_world))
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
         val btn = findViewById<Button>(R.id.btn)
-btn.setOnClickListener { startMainActivity2(this,getString(R.string.hello_world_2))
-}
+        btn.setOnClickListener {
+            startMainActivity2(this, getString(R.string.hello_world_2))
+        }
         supportActionBar?.title = " New title for MainActivity"
         val textView = findViewById<TextView>(R.id.textView)
 
 
-
         val btnGoToActivity2 = findViewById<Button>(R.id.btnGoToActivity2)
-        btnGoToActivity2.setOnClickListener{
-            startActivity(Intent(this, MainActivity2::class.java)
-                .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
-                .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-            //MainActivity2 will not be added to the backstack
+        btnGoToActivity2.setOnClickListener {
+            startActivity(
+                Intent(this, MainActivity2::class.java)
+                    .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+                    .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                //MainActivity2 will not be added to the backstack
 //                .addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY)
 
             )
         }
 
     }
-
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        val inflater = menuInflater
-        inflater.inflate(R.menu.item_menu,menu)
-        return super.onCreateOptionsMenu(menu)
-    }
-
-
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (item.itemId == android.R.id.home){
-
-            Toast.makeText(this,"clicked back arrow", Toast.LENGTH_SHORT).show()
-//            onBackPressed()
-        }
-//        if (item.itemId = R.id.close_app){
-//            finishAffinity()
-//        }
-
-        return super.onOptionsItemSelected(item)
-    }
-
 
 }
 
--- app/src/main/java/com/example/androidproject/MainActivity2.kt
+++ app/src/main/java/com/example/androidproject/MainActivity2.kt
@@ -4,12 +4,12 @@ import android.content.Context
 import android.content.Intent
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import android.view.MenuItem
 import android.widget.Button
 import android.widget.TextView
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,38 +18,46 @@ class MainActivity2 : AppCompatActivity() {
         val btnGoToActivity3 = findViewById<Button>(R.id.btnGoToActivity3)
         btnGoToActivity3.setOnClickListener { startActivity(Intent("com.example.androidproject.@+id/btnGoToActivity3")) }
         supportActionBar?.setBackgroundDrawable(ColorDrawable(Color.parseColor(getString(R.string.purple))))
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-
-       // supportActionBar?.setBackgroundDrawable(R.drawable.cat)
+        // supportActionBar?.setBackgroundDrawable(R.drawable.cat)
 
         val textView2 = findViewById<TextView>(R.id.textView2)
 
-     //   val  data  = intent.getShortExtra("KEY")?: " no data"
-    //    textView2.text = data
+        val data: String = intent.getStringExtra(KEY) ?: getString(R.string.no_data)
+        textView2.text = data
 
-val intentExtras =intent.extras?.get("greeting")
-        Toast.makeText(this,"$intentExtras", Toast.LENGTH_SHORT).show()
-
-        }
-
-    companion object{
-        private  const val GREETING = "GREETING"
-        fun startMainActivity2(context: Context, string: String){
-            val intent = Intent(context, MainActivity2::class.java)
-            intent.putExtra("greeting","hello world")
-            context.startActivity(intent)
-        }
-
-
+//val intentExtras =intent.extras?.get("greeting")
+//        Toast.makeText(this,"$intentExtras", Toast.LENGTH_SHORT).show()
 
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.item_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> onBackPressed()
+            R.id.close_app -> finishAffinity()
+            R.id.goBack -> onBackPressed()
+        }
+        return super.onOptionsItemSelected(item)
+    }
 
+    companion object {
+        private const val KEY = "1"
+        fun startMainActivity2(context: Context, string: String) {
+            val intent = Intent(context, MainActivity2::class.java)
+            intent.putExtra(KEY, string)
+            context.startActivity(intent)
+        }
 
+    }
 
-
-  }
+}
 
 
 
